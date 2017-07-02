15f45dc57a09c9d80b3f6c406094aa3697c1f2fe
Nick Fehlinger <nfehlinger@gmail.com>
2017-07-02 12:18:12 -0400

    Some work I did on the layout yesterday, but more importantly I got github to workgit push origin nick

--- server.rb
+++ server.rb
@@ -2,12 +2,31 @@ require 'sinatra'
 require 'sinatra/activerecord'
 set :database, {adapter: "sqlite3", database: "db/wineCan.db"}
 require './models'
+require 'sinatra/flash'
+require 'bundler/setup'
+enable :sessions
+
+before do
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+	end
+end
 
 get '/' do
-  # @users = User.all
+  
   erb :home
 end
 
+post '/sign_in' do
+	@user = User.find_by(email: params[:username])
+	if @user.password == params[:password]
+		session[:user_id] = @user.id
+		redirect '/'
+	else
+		flash[:alert] = "username/password combination does not exist"
+		redirect '/'
+	end
+end
 # get '/user/:id' do
 #   @user = User.find(params[:id])
 #   erb :profile
