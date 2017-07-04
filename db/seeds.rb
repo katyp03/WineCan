95d6fed6b98c294d1663c8bb53ff3efedc5c3424
Katy Pola <katyp03@gmail.com>
2017-07-04 16:42:53 -0400

    Started homepage styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Avatar.create([
 # Create our users
 User.create([
   {id: 1, email: 'nfehlinger@gmail.com', password: 'password', username: 'nickfehlinger', bio: 'I\'m pretty drunk right now', hometown: 'New York, NY', avatar_id: 1},
-  {id: 2, email: 'katy@idontknowyouremail.com', password: 'password', username: 'katyp03', bio: 'I\'m pretty drunk right now', hometown: 'New York, NY', avatar_id: 2}
+  {id: 2, email: 'katyp03@gmail.com', password: 'password', username: 'katyp03', bio: 'I\'m pretty drunk right now', hometown: 'New York, NY', avatar_id: 2}
 ])
 # posts
 Post.create([
